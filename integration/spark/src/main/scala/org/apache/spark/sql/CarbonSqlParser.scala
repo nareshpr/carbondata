1f2ae81a3ccd6e257964bdc389e305939ed5dc6e
nareshpr <prnaresh.naresh@gmail.com>
2016-05-30 18:57:31 +0530

    When Complex datatype is provided in DICTIONARY_EXCLUDE, validated and throwing unsupported exception.

--- integration/spark/src/main/scala/org/apache/spark/sql/CarbonSqlParser.scala
+++ integration/spark/src/main/scala/org/apache/spark/sql/CarbonSqlParser.scala
@@ -556,6 +556,11 @@ class CarbonSqlParser()
             val errormsg = "DICTIONARY_EXCLUDE column: " + dictExcludeCol +
               " is no exist in table. Please check create table statement."
             throw new MalformedCarbonCommandException(errormsg)
+          } else if (isComplexDimDictionaryExclude(fields.filter(x =>
+              x.column.equalsIgnoreCase(splittedCol.trim))(0).dataType.get)) {
+            val errormsg = "DICTIONARY_EXCLUDE is unsupported for complex datatype column: " +
+              splittedCol.trim
+            throw new MalformedCarbonCommandException(errormsg)
           }
         }
     }
@@ -624,6 +629,14 @@ class CarbonSqlParser()
   def isDetectAsDimentionDatatype(dimensionDatatype: String): Boolean = {
     val dimensionType = Array("string", "array", "struct", "timestamp")
     dimensionType.exists(x => x.equalsIgnoreCase(dimensionDatatype))
+  }
+
+  /**
+   * detects whether complex dimension is part of dictionary_exclude
+   */
+  def isComplexDimDictionaryExclude(dimensionDataType: String): Boolean = {
+    val dimensionType = Array("array", "struct")
+    dimensionType.exists(x => x.equalsIgnoreCase(dimensionDataType))
   }
 
   /**
